5874d55826ec70adeb2fc0c03571fcd08542cb7e
Kevin COSTA <kcosta@e3r6p6.42.fr>
2016-12-01 15:04:48 +0100

    Commit

---
+++ libft/includes/ft_utils.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcosta <kcosta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/30 17:13:35 by kcosta            #+#    #+#             */
+/*   Updated: 2016/11/30 17:19:57 by kcosta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_UTILS_H
+# define FT_UTILS_H
+
+# ifndef HEX
+#  define HEX "0123456789abcdef"
+# endif
+
+# ifndef BUFF_SIZE
+#  define BUFF_SIZE 1024
+# endif
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+
+int		ft_getline(const int fd, char **line);
+
+#endif
---
+++ libft/srcs/list/ft_lstnew.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcosta <kcosta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/04 15:15:51 by kcosta            #+#    #+#             */
+/*   Updated: 2016/11/30 16:25:07 by kcosta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include "ft_memory.h"
+
+t_list	*ft_lstnew(const void *content, size_t content_size)
+{
+	t_list	*list;
+
+	if (!(list = (t_list*)ft_memalloc(sizeof(t_list))))
+		return (NULL);
+	if (!content)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		if (!(list->content = ft_memalloc(sizeof(content_size))))
+			return (NULL);
+		ft_memcpy(list->content, content, content_size);
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
+}
